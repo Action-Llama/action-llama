86d837cecd747655daab1bca9e925b1d310cef23
asselstine <brendan@g9software.xyz>
2026-03-22 20:57:54 +0000

    fix: restore missing tar-fs import in harness.ts

--- packages/e2e/src/harness.ts
+++ packages/e2e/src/harness.ts
@@ -1,6 +1,7 @@
 import Docker from "dockerode";
 import { randomUUID } from "crypto";
 import { generateKeyPairSync } from "crypto";
+import tar from "tar-fs";
 import { promises as fs } from "fs";
 import path from "path";
 import { fileURLToPath } from 'url';
